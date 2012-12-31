8b0e0b057527df9f0886985500cf55fae43708ff
z12058ts <z12058ts@z12058ts-VirtualBox.(none)>
2012-12-31 17:31:40 +0900

    generated myapp.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '18edcc45d3c07341eaf261fd84c4a023f2ca892711fbc1a27dc635c7d4274fe5982af8469c24d07034ae6c72033df2fd50a017eef54559f49f7305c31da1bdba'
